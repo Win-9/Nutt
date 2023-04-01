dbe728881aec52d7a3859a99e488f3c189f22755
win9 <krrr883@gmail.com>
2023-04-02 00:47:28 +0900

    feat: 체중증감량에 따른 일일 필요칼로리 계산 알고리즘 구성

---
+++ src/main/java/com/backend/nutt/service/AchieveService.java
@@ -0,0 +1,55 @@
+package com.backend.nutt.service;
+
+import com.backend.nutt.domain.Achieve;
+import com.backend.nutt.domain.Member;
+import com.backend.nutt.dto.request.AchieveCheckRequest;
+import com.backend.nutt.dto.request.AchieveSetRequest;
+import com.backend.nutt.dto.response.DailyAchieveResponse;
+import com.backend.nutt.repository.AchieveRepository;
+import com.backend.nutt.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AchieveService {
+    private final AchieveRepository achieveRepository;
+    private final MemberRepository memberRepository;
+
+    public DailyAchieveResponse calculateKcal(AchieveSetRequest achieveSetRequest, Member member) {
+        String gender = String.valueOf(member.getGender());
+        double bmr = getBmr(member, gender);
+        double tdee = bmr * achieveSetRequest.getPal();
+        double dailyTargetKcal = targetKcal(achieveSetRequest.getTarget(),
+                achieveSetRequest.getWeightGainRate(), tdee);
+
+        double carbohydrate = dailyTargetKcal * 0.45;
+        double protein = dailyTargetKcal * 0.25;
+        double fat = dailyTargetKcal * 0.30;
+
+        Achieve achieve = Achieve.builder()
+                .achieveKcal(dailyTargetKcal)
+                .achieveCarbohydrate(carbohydrate)
+                .achieveFat(fat)
+                .achieveProtein(protein)
+                .build();
+
+        achieveRepository.save(achieve);
+
+        return DailyAchieveResponse.build(dailyTargetKcal, carbohydrate, protein, fat);
+    }
+
+    private double targetKcal(String target, double weightGainRate, double tdee) {
+        switch (target) {
+            case "loss":
+                return tdee - (weightGainRate * 7700 / 7);
+            case "maintenance":
+                return tdee;
+            case "increase":
+                return tdee + (weightGainRate * 7700 / 7);
+            default:
+                throw new IllegalArgumentException("Not_Valid_Option");
+        }
+    }
+
+}
