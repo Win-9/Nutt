13ee513e13141ec7b709f57066d7d2a887a9836b
win9 <krrr883@gmail.com>
2023-04-04 00:47:35 +0900

    Feat: 모든필드 생성자 추가

--- src/main/java/com/backend/nutt/dto/request/FormLoginUserRequest.java
+++ src/main/java/com/backend/nutt/dto/request/FormLoginUserRequest.java
@@ -1,8 +1,10 @@
 package com.backend.nutt.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class FormLoginUserRequest {
     private String email;
     private String password;
