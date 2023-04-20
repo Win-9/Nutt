6814e646e7c9fbad96579efdef04e545fbfa1a7e
win9 <krrr883@gmail.com>
2023-04-21 03:42:28 +0900

    Feat: 에러시의 응답처리 분리

--- src/main/java/com/backend/nutt/exception/badrequest/BadRequestException.java
+++ src/main/java/com/backend/nutt/exception/badrequest/BadRequestException.java
@@ -1,8 +1,9 @@
 package com.backend.nutt.exception.badrequest;
 
-import com.backend.nutt.common.ResponseMessage;
 import com.backend.nutt.exception.ErrorMessage;
+import lombok.Getter;
 
+@Getter
 public class BadRequestException extends IllegalArgumentException {
     private ErrorMessage errorMessage;
 
--- src/main/java/com/backend/nutt/exception/notfound/UserException.java
+++ src/main/java/com/backend/nutt/exception/notfound/UserException.java
@@ -1,8 +1,10 @@
 package com.backend.nutt.exception.notfound;
 
 import com.backend.nutt.exception.ErrorMessage;
+import lombok.Getter;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Getter
 public class UserException extends UsernameNotFoundException
 {
     private ErrorMessage errorMessage;
