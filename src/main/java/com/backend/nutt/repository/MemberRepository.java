2a533b393c0e40c637bdf911128c0f11cffa48f8
win9 <krrr883@gmail.com>
2023-05-24 00:37:44 +0900

    Feat: 섭취정보를 응답시 사용자의 목표치를 보여주도록 수정

--- src/main/java/com/backend/nutt/repository/MemberRepository.java
+++ src/main/java/com/backend/nutt/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.backend.nutt.repository;
 
+import com.backend.nutt.domain.Achieve;
 import com.backend.nutt.domain.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +10,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(String Id);
 
-    @EntityGraph(attributePaths = "mealPlanList")
+    @EntityGraph(attributePaths = {"mealPlanList", "achieve"})
     Optional<Member> findByEmail(String email);
 
     boolean existsMemberByEmail(String email);
