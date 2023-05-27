ec2a584cae62028cf760cb2488a547a336193b9c
win9 <krrr883@gmail.com>
2023-05-27 16:14:09 +0900

    Feat: ObejctStorage에서 S3로 변경으로 인한 설정변경

--- src/main/java/com/backend/nutt/config/S3Config.java
+++ src/main/java/com/backend/nutt/config/S3Config.java
@@ -2,7 +2,6 @@ package com.backend.nutt.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,15 +19,12 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.s3.endpoint}")
-    private String endPoint;
-
     @Bean
     public AmazonS3 amazonS3Client() {
-        return AmazonS3ClientBuilder
-                .standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, region))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
                 .build();
     }
 
