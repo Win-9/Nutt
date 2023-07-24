1ea06d012b1a6c338fbf55664ab41dbd8011c498
win9 <krrr883@gmail.com>
2023-07-24 15:35:51 +0900

    Test: 일일섭취기록 BDD에맞춰서 테스트 수정

--- src/test/java/com/backend/nutt/service/DailyIntakeServiceTest.java
+++ src/test/java/com/backend/nutt/service/DailyIntakeServiceTest.java
@@ -1,8 +1,10 @@
 package com.backend.nutt.service;
 
+import com.backend.nutt.domain.Achieve;
 import com.backend.nutt.domain.MealPlan;
 import com.backend.nutt.domain.Member;
 import com.backend.nutt.domain.type.IntakeTitle;
+import com.backend.nutt.domain.type.Role;
 import com.backend.nutt.dto.request.IntakeFormRequest;
 import com.backend.nutt.repository.IntakeRepository;
 import com.backend.nutt.repository.MealPlanRepository;
@@ -11,50 +13,74 @@ import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
+import static org.mockito.BDDMockito.*;
 import static org.mockito.ArgumentMatchers.*;
 
 @SpringBootTest
 class DailyIntakeServiceTest {
 
-    @Autowired
+    @Mock
     private IntakeRepository intakeRepository;
 
-    @Autowired
+    @Mock
     private MealPlanRepository mealPlanRepository;
 
-    @Autowired
+    @InjectMocks
     private DailyIntakeService dailyIntakeService;
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
 
     @Test
     @DisplayName("존재하지 않는 식단 식사 저장")
     @Transactional
     public void saveDailyIntakeWithMealPlan() {
+        //given
         IntakeFormRequest request = generateIntakeRequest();
         Member member = generateMember();
+
+        when(mealPlanRepository.findByMemberIdAndIntakeDateAndIntakeTitle(anyLong(), anyInt(), anyInt(), any(IntakeTitle.class)))
+                .thenReturn(null);
+
+        //when
         MealPlan findMealPlan = dailyIntakeService.saveDailyIntake(member, request, anyString());
 
+        //then
+        verify(mealPlanRepository, times(1)).findByMemberIdAndIntakeDateAndIntakeTitle(anyLong(), anyInt(), anyInt(), any(IntakeTitle.class));
+
         Assertions.assertThat(findMealPlan.getIntakeList().size()).isEqualTo(1);
-        Assertions.assertThat(findMealPlan.getMember().getName()).isEqualTo("name");
+        Assertions.assertThat(findMealPlan.getMember().getEmail()).isEqualTo("test@naver.com");
+        Assertions.assertThat(findMealPlan.getMember().getPassword()).isEqualTo("abcdeftgas12!");
     }
 
     @Test
     @DisplayName("존재하는 식단 식사 저장")
     @Transactional
     public void saveDailyIntakeWithNoMealPlan() {
+        //given
         IntakeFormRequest request = generateIntakeRequest();
         Member member = generateMember();
         MealPlan mealPlan = generateMealPlan(member);
+
+        when(mealPlanRepository.findByMemberIdAndIntakeDateAndIntakeTitle(anyLong(), anyInt(), anyInt(), any(IntakeTitle.class)))
+                .thenReturn(mealPlan);
+
+        //when
         MealPlan findMealPlan = dailyIntakeService.saveDailyIntake(member, request, anyString());
+
+
+        //then
+        verify(mealPlanRepository, times(1)).findByMemberIdAndIntakeDateAndIntakeTitle(anyLong(), anyInt(), anyInt(), any(IntakeTitle.class));
 
         Assertions.assertThat(findMealPlan.getIntakeList().size()).isEqualTo(1);
         Assertions.assertThat(findMealPlan.getIntakeList().size()).isEqualTo(mealPlan.getIntakeList().size());
@@ -68,37 +94,31 @@ class DailyIntakeServiceTest {
                 .intakeTitle(IntakeTitle.BREAKFAST)
                 .build();
         mealPlan.setMember(member);
-        return mealPlanRepository.save(mealPlan);
+        return mealPlan;
     }
 
     private Member generateMember() {
-        Member member = Member.builder()
-                .name("name")
-                .email(anyString())
-                .password(anyString())
-                .age(anyInt())
-                .gender(any())
-                .role(any())
-                .nickName(anyString())
-                .height(anyDouble())
-                .weight(anyDouble())
-                .achieve(any())
+        return Member.builder()
+                .id(1L)
+                .email("test@naver.com")
+                .role(Role.NORMAL)
+                .password("abcdeftgas12!")
                 .mealPlanList(new ArrayList<>())
+                .achieve(Achieve.builder().build())
                 .build();
-        return memberRepository.save(member);
     }
 
     private IntakeFormRequest generateIntakeRequest() {
         return new IntakeFormRequest(
-                IntakeTitle.BREAKFAST.toString(),
-                anyString(),
-                anyDouble(),
-                anyDouble(),
-                anyDouble(),
-                anyDouble(),
-                anyString(),
-                anyString(),
-                anyString()
+                "BREAKFAST",
+                "raw String",
+                0.1,
+                0.1,
+                0.1,
+                0.1,
+                "raw String",
+                "raw String",
+                "raw String"
         );
     }
 }
