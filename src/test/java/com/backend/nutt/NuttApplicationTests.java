5b90cd4e594d665dd42bf93d351874d32cfe9324
win9 <krrr883@gmail.com>
2023-07-30 18:06:32 +0900

    Test: Mocking 테스트로 수정

--- src/test/java/com/backend/nutt/NuttApplicationTests.java
+++ src/test/java/com/backend/nutt/NuttApplicationTests.java
@@ -3,7 +3,7 @@ package com.backend.nutt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class NuttApplicationTests {
 
 	@Test
